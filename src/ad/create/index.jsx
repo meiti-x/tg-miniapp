6353f70b31491dec8b203797660b8e77bac0b413
MehdiMomeni <mehdimomeni@Meiti.local>
2024-11-11 22:36:53 +0330

    feat: add call api

--- src/ad/create/index.jsx
+++ src/ad/create/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-
+import axios from "axios";
 export default function CreateAdForm() {
   const {
     register,
@@ -11,6 +11,7 @@ export default function CreateAdForm() {
   const onSubmit = (data) => {
     console.log(data);
     // Send data to your backend here
+    axios.get("http://qbc8.boloorin.top:8080/healthz");
   };
 
   return (
